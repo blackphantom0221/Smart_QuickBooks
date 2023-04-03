e7c0073bb1664c5c87a22b2f22e50909552d1f95
blackphantom0221 <blackphantom0221@gmail.com>
2023-04-03 01:39:10 -0400

    Init server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-    res.send("Hello, world!");
+const port = process.env.HTTP_PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port ${port || process.env.PORT}`);
 });

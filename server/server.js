de2744ba855e58b90b9a6be3d9c6fb112eaedac3
blackphantom0221 <blackphantom0221@gmail.com>
2023-04-05 07:00:14 -0400

    setup EJS, add error handling

--- server/server.js
+++ server/server.js
@@ -2,19 +2,32 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+const ejs = require("ejs");
 require("dotenv").config();
-
-const port = process.env.HTTP_PORT;
 
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
 
 //Setup these environment variables!
 const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+  if (err) {
+    app.get("/", (req, res) => {
+      res.send("Error connecting to database");
+    });
+    console.log(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
 app.post("/input", (req, res) => {
@@ -24,19 +37,21 @@ app.post("/input", (req, res) => {
   const emailAddress = req.body.emailAddress;
 
   db.query(
-      "INSERT INTO phonebook (firstName, lastName, phoneNumber , emailAddress) VALUES (?,?,?,?)",
-      [firstName, lastName, phoneNumber, emailAddress],
-      (err, result) => {
-          if (err) console.log(err);
-          else res.send("Values Inserted");
-      }
+    "INSERT INTO phonebook (firstName, lastName, phoneNumber , emailAddress) VALUES (?,?,?,?)",
+    [firstName, lastName, phoneNumber, emailAddress],
+    (err, result) => {
+      if (err) console.log(err);
+      else res.send("Values Inserted");
+    }
   );
 });
 
 app.get("/view", (req, res) => {
   db.query("SELECT * FROM phonebook", (err, result) => {
-      if (err) console.log(err);
-      else res.send(result);
+    if (err) {
+      console.log(err);
+      res.send("Error connecting to database.");
+    } else res.send(result);
   });
 });
 
@@ -47,30 +62,34 @@ app.put("/update", (req, res) => {
   const phoneNumber = req.body.phoneNumber;
   const emailAddress = req.body.emailAddress;
   db.query(
-      "UPDATE phonebook SET firstName = ? , lastName = ? , phoneNumber = ? , emailAddress = ? where id = ?",
-      [firstName, lastName, phoneNumber, emailAddress, id],
-      (err, result) => {
-          if (err) console.log(err);
-          else res.send(result);
-      }
+    "UPDATE phonebook SET firstName = ? , lastName = ? , phoneNumber = ? , emailAddress = ? where id = ?",
+    [firstName, lastName, phoneNumber, emailAddress, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send("Error connecting to database.");
+      } else res.send(result);
+    }
   );
 });
 
 app.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
   db.query("DELETE FROM phonebook WHERE id = ?", id, (err, result) => {
-      if (err) console.log(err);
-      else res.send(result);
+    if (err) console.log(err);
+    else res.send(result);
   });
 });
 
 app.delete("/deleteAll", (req, res) => {
   db.query("truncate phonebook", (err, result) => {
-      if (err) console.log(err);
-      else res.send(result);
+    if (err) {
+      console.log(err);
+      res.send("Error connecting to database.");
+    } else res.send(result);
   });
 });
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server is running on port ${port || process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });

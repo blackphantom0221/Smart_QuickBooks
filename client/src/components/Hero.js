5b2f0f36fec677d420424d26899c6ab268865fb3
blackphantom0221 <blackphantom0221@gmail.com>
2023-04-05 05:52:07 -0400

    add EditRow and Hero components

---
+++ client/src/components/Hero.js
@@ -0,0 +1,34 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import heroImg from "../assets/heroPhone.svg";
+import "../css/hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <Container className="vh-100">
+        <Row className="mt-3">
+          <Col sm={6} className="d-flex align-items-center">
+            <div>
+              <h1>
+                Manage Contacts in a{" "}
+                <span style={{ color: "#CCC3F8" }}>Quick Book!</span>
+              </h1>
+              <p>Instantly export your contacts as a CSV file.</p>
+              <Link to="/input">
+                <Button className="heroBtn" variant="outline-primary" size="lg">
+                  Get Started
+                </Button>
+              </Link>
+            </div>
+          </Col>
+          <Col sm={6} className="d-flex align-items-center justify-content-end">
+            <img src={heroImg} alt="Quickbooks" width="100%" height="85%" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
